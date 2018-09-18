e07f783f1445c3d9c510b5925112f83e529e2b77
Musab Bairat <musab.bairat@atos.net>
2018-09-18 13:22:31 +0200

    cleaned the project

--- src/main/java/org/atos/scouter/Main/Main.java
+++ src/main/java/org/atos/scouter/Main/Main.java
@@ -4,11 +4,7 @@ package org.atos.scouter.Main;
 
 import org.apache.commons.cli.MissingArgumentException;
 import org.atos.scouter.TikaTools.PdfExtracter;
-import org.atos.scouter.metrics.MetricsLogger;
-import org.atos.scouter.util.ConceptTopologyUtility;
-import org.atos.scouter.util.MetricsKeys;
 import org.atos.scouter.util.PropertiesManager;
-import org.atos.scouter.util.TopicExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,8 +16,6 @@ public class Main {
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-
-    static MetricsLogger METRICS_LOGGER;
 
 
     /**
@@ -42,23 +36,6 @@ public class Main {
 
         PdfExtracter extracter = new PdfExtracter();
         extracter.extractText(PROPERTIES_MANAGER.getProperty("pdf.root.path"));
-
-//        initializeConceptTopology();
-//        buildTopicExtractionModel();
-
-    }
-
-    private static void initializeConceptTopology(){
-        ConceptTopologyUtility.initilizeInputTopology();
-        ConceptTopologyUtility.printKeywordsScoresMap();
-    }
-
-    private static void buildTopicExtractionModel() throws Exception {
-        TopicExtractor topicExtractor = TopicExtractor.getInstance();
-
-        long trainingStartTime = System.currentTimeMillis();
-        topicExtractor.trainTopicExtractionModel();
-        METRICS_LOGGER.log(MetricsKeys.TOPIC_EXTRACTION_TRAINING_TIME, System.currentTimeMillis() - trainingStartTime);
     }
 
 }
--- src/main/java/org/atos/scouter/sentiment/ISentimentProcessor.java
+++
@@ -1,5 +0,0 @@
-package org.atos.scouter.sentiment;
-
-public interface ISentimentProcessor {
-	String analyzeSentiment(String sentence);
-}
